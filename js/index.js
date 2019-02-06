f35a5786b476c3bda64ace7b7b47bc9b534faad8
Kateryna Venikova <kate.venik.i@gmail.com>
2019-02-06 16:53:09 +0000

    DOM,jQuery

--- js/index.js
+++ js/index.js
@@ -59,3 +59,21 @@ const clockContainer = document.querySelector('.clock');
 setInterval(function() {
     clockContainer.innerText = (new Date()).toLocaleTimeString();
 }, 10);
+
+
+
+//HW
+const display = document.querySelector('.calculator input[name=display]');
+
+document.querySelectorAll('.digits, .opers')
+ .forEach( btn => btn.addEventListener('click', digitOperPressed) );
+ 
+function digitOperPressed(e) {
+    display.value += e.target.innerText;
+}
+
+document.querySelector('.result')
+    .addEventListener('click', function(){
+        display.value = eval(display.value);       
+    })
+//HW
